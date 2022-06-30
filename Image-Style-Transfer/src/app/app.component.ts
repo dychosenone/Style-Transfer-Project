f7afee358c1522a8ee2895b4f3d0c5fc5355bfc4
NobleTheNoble <57493855+NobleTheNoble@users.noreply.github.com>
2022-06-30 13:52:46 +0800

    removed fancy buttons

--- Image-Style-Transfer/src/app/app.component.ts
+++ Image-Style-Transfer/src/app/app.component.ts
@@ -28,7 +28,26 @@ import {
           style({ opacity: 0})
         )
       ])
-    ])
+    ]),
+
+    trigger("slide_animation", [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateY(-100%)"}), //apply default styles before animation starts
+        animate(
+          "500ms ease-in-out",
+          style({ opacity: 1, transform: "translateY(0%)"})
+        )
+      ]),
+      transition(":leave", [
+        style({ opacity: 1, transform: "translateY(0%)"}), //apply default styles before animation starts
+        animate(
+          "500ms ease-in-out",
+          style({ opacity: 0, transform: "translateY(-100%)"})
+        )
+      ])
+    ]),
+
+    
   ],
 })
 export class AppComponent {
@@ -36,11 +55,32 @@ export class AppComponent {
   Page1_Display = true;
   Page2_Display = false;
 
+  Page2_Content_Display = false;
+  Page2_Style_Display = false;
+
 
   async Start(){
     this.Page1_Display = !this.Page1_Display;
     await new Promise(f => setTimeout(f, 510));
     this.Page2_Display = !this.Page2_Display;
+
+    await new Promise(f => setTimeout(f, 510));
+
+    this.Page2_Content_Display = !this.Page2_Content_Display;    
+    this.Page2_Style_Display = !this.Page2_Style_Display;  
+  }
+
+
+  async Back(){
+    this.Page2_Content_Display = !this.Page2_Content_Display;    
+    this.Page2_Style_Display = !this.Page2_Style_Display;  
+
+    await new Promise(f => setTimeout(f, 510));
+
+    this.Page2_Display = !this.Page2_Display;
+    await new Promise(f => setTimeout(f, 510));
+
+    this.Page1_Display = !this.Page1_Display;
   }
 
   
