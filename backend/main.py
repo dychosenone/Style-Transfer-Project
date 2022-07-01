9192612d36217b219ed229bd4258ea58105e267e
dychosenone <jacobmigueldy@gmail.com>
2022-07-01 11:47:28 +0800

    added backend code

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,53 @@
+from fileinput import filename
+import os
+from click import style
+
 from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug.utils import secure_filename
+
+from model.cnn_model import Model
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'jpg', 'png'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello():
-    return "Hello, World"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/processImage', methods=["POST"])
+def processImage():
+    
+    '''
+    Todo: 
+    - Get Image
+    - Send Image to Backend for Processing
+    - Return Image
+    '''
+    if request.method == "POST":
+
+        contentFile = request.files['content']
+        styleFile = request.files['style']
+
+        if contentFile and allowed_file(contentFile.filename):
+            styleFileName = secure_filename(contentFile.filename)
+            contentFile.save(os.path.join(app.config['UPLOAD_FOLDER'], contentFileName))
+
+
+        if styleFile and allowed_file(styleFile.filename):
+            contentFileName = secure_filename(style.filename)
+            styleFile.save(os.path.join(app.config['UPLOAD_FOLDER'], styleFileName))
+
+        model = Model()
+        model(styleFileName, contentFileName)
+        
+
+
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/model/cnn_model.py
+++ backend/model/cnn_model.py
@@ -1,6 +1,16 @@
+from mimetypes import init
+from click import style
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ContentLoss(nn.Module):
 
@@ -27,3 +37,47 @@ class StyleLoss(nn.Module):
         G = torch.bmm(F, F.transpose(1,2)) 
         G.div_(h*w)
         return G
+
+class Model():
+
+    def __init__(self, content_img, style_img) -> None:
+        self.cnn = models.vgg19(pretrained=True)
+
+        self.norm_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+        self.norm_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+        self.image_size = 128
+        self.loader = transforms.Compose([transforms.Resize(self.image_size),
+                transforms.ToTensor()])
+
+        self.unloader = transforms.ToPILImage()
+
+    def __call__(self, content_img, style_img) -> None:
+
+        styleImage = self.loadImage(style_img)
+        contentImage = self.loadImage(content_img)
+
+        plt.figure()
+        self.imshow(styleImage)
+
+        pass
+
+    # View Image Function
+        
+    def imshow(self, tensor, title=None):
+        image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+        image = image.squeeze(0)      # remove the fake batch dimension
+        image = self.unloader(image)
+        plt.imshow(image)
+        if title is not None:
+            plt.title(title)
+        plt.pause(0.001) # pause a bit so that plots are updated
+
+    # Load Image Function
+    def loadImage(self, path):
+        image = Image.open(path)
+        image = self.loader(image).unsqueeze(0)
+
+        return image.to(device, torch.float)
+
+    
