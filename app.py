cee650c905cda82695f0b4d7e4ebbd018709524c
NobleTheNoble <57493855+NobleTheNoble@users.noreply.github.com>
2022-07-03 15:47:02 +0800

    file upload now present

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 #.\env\Scripts\activate.bat
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+CONTENT_UPLOAD_FOLDER = 'content'
+STYLE_UPLOAD_FOLDER = 'style'
+
+app.secret_key = "secret key"
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -11,6 +22,13 @@ def index():
 @app.route('/inputpage/')
 def inputpage():
     return render_template('p2.html')
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def uploadFile():
+    if request.method == 'POST':
+      f = request.files['file']
+      f.save(CONTENT_UPLOAD_FOLDER + '/' + secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == "__main__":
     app.run(debug = True)
--- static/js/script-p2.js
+++ static/js/script-p2.js
@@ -17,5 +17,7 @@ $(document).ready(function(){
             })
         });
     })
+
+   
    
 })
