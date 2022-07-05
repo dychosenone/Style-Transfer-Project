de020298ba2ee42410b0376ea72b21581ebcd10d
NobleTheNoble <57493855+NobleTheNoble@users.noreply.github.com>
2022-07-05 12:22:26 +0800

    p1 request

--- static/js/script-p1.js
+++ static/js/script-p1.js
@@ -2,10 +2,26 @@ $(document).ready(function(){
     //$(window).on('load', function() {
         $("#head").delay(500).animate({"opacity": "1"}, 400, function(){
             $("#body").delay(100).animate({"opacity": "1"}, 400, function(){
-                $("#start").delay(100).animate({"opacity": "1"}, 400);
+                $("#start").delay(100).animate({"opacity": "1"}, 400, function(){
+                    $("#input").delay(100).animate({"opacity": "1"}, 400);
+                    $(".submit-block").delay(100).animate({"opacity": "1"}, 400);
+                });
             });
         }); 
     //});
+
+    $('.input-text').on('input', function(e){
+        $(this).css({'width' : ($(this).val().length + 0.5) + 'em'})
+    })
+     
+    $('#submit').click(function(){
+        if ($('.input-text').val().length == 0){
+            alert('Please input your code!')
+        }
+        else{
+            //code here..
+        }
+    })
 
     $("#start_transfer").click(function(){
         $("#start_transfer").css(
